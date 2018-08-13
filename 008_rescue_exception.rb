63baa990ca499ab5125a5c2ea1ad61619c38b5fb
Thomas Brennetot <thomas.brennetot@gmail.com>
2018-08-13 16:42:58 +0900

    Update syntax

--- 008_rescue_exception.rb
+++ 008_rescue_exception.rb
@@ -2,19 +2,17 @@
 # http://apidock.com/ruby/Exception
 
 def raising_error
-  begin
-    yield
-  rescue ArgumentError => expection_1
-    puts "Rescue from ArgumentError: #{expection_1}"
-  rescue SecurityError => expection_2
-    puts "Rescue from SecurityError: #{expection_2}"
-  rescue => expection_3
-    puts "Rescue any exception. Should not be used, it's hiding errors. #{expection_3}"
-  else
-    puts 'Run if no exception raised'
-  ensure
-    puts 'This line always run', ''
-  end
+  yield
+rescue ArgumentError => expection_1
+  puts "Rescue from ArgumentError: #{expection_1}"
+rescue SecurityError => expection_2
+  puts "Rescue from SecurityError: #{expection_2}"
+rescue => expection_3
+  puts "Rescue any exception. Should not be used, it's hiding errors. #{expection_3}"
+else
+  puts 'Run if no exception raised'
+ensure
+  puts 'This line always run', ''
 end
 
 raising_error { puts "No error raised" }
